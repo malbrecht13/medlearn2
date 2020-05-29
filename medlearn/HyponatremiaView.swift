b8d3e236b93c3910fa3c0ac49bdea303bebf4f92
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2020-05-29 07:04:54 -0500

    Added HyponatremiaView.swift

---
+++ medlearn/HyponatremiaView.swift
@@ -0,0 +1,21 @@
+//
+//  HyponatremiaView.swift
+//  medlearn
+//
+//  Created by Michael Albrecht on 5/29/20.
+//  Copyright © 2020 Michael Albrecht. All rights reserved.
+//
+
+import SwiftUI
+
+struct HyponatremiaView: View {
+    var body: some View {
+        Text("Hyponatremia")
+    }
+}
+
+struct HyponatremiaView_Previews: PreviewProvider {
+    static var previews: some View {
+        HyponatremiaView()
+    }
+}
