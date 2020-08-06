64ba71d1ad3d97ed7200995fd635c016d323a4fc
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2020-08-05 22:38:45 -0500

    Created a generic ShowPicker view to represent many different Segmented Pickers

--- medlearn/Views/Hyponatremia Causes/HyponatremiaCausesView.swift
+++ medlearn/Views/Hyponatremia Causes/HyponatremiaCausesView.swift
@@ -10,13 +10,15 @@ import SwiftUI
 
 struct HyponatremiaCausesView: View {
     
-    @State private var plasmaSelection: Int = 0
+    @State private var plasmaSelection: Int = 1
     @State private var urineSelection: Int = 0
-    @State private var volumeSelection: Int = 0
+    @State private var volumeSelection: Int = 1
     @State private var urineNaSelection: Int = 0
     
-    @State var showingDetail = false
-    @State var showReferences = false
+    @State private var showingDetail = false
+    @State private var showReferences = false
+    
+    @State private var half = false
     
     let plasmaOsm = ["<275 mOsm/L", "275-295 mOsm/L", ">295 mOsm/L"]
     let urineOsm = ["<100 mOsm/L", ">100 mOsm/L"]
@@ -28,139 +30,111 @@ struct HyponatremiaCausesView: View {
     let lineWidth: CGFloat = 3
     
     var body: some View {
-            ZStack {
-                        Color(.gray).opacity(0.1).edgesIgnoringSafeArea(.all)
+        ZStack {
+            Color(.gray).opacity(0.1).edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 40) {
+                
+                Text("Hyponatremia: Determine Cause")
+                    .modifier(MyTitleModifier(fillColor: fillColor))
+                
+                Text("Instructions:  You may need plasma osmolality, urine osmolality, urine sodium, and patient's volume status for this.  Different options will appear depending on your selections below. The Go! button is pressable at any time and will present a different set of causes depending on your selections.")
+                    .font(.footnote)
+                    .padding(.horizontal, 20)
+                
+                ShowPicker(parentBinding: $plasmaSelection, text: "Select plasma osmolality", parentArray: plasmaOsm)
+                
+                //only display options below "select serum osmolality
+                //if plasmaSelection is option 0
+                
+                if self.plasmaSelection == 0 {
+                    
+                    ShowPicker(parentBinding: $urineSelection, text: "Select urine Osmolality", parentArray: urineOsm)
+                    
+                    //only display these Picker options if urineOsmolality is
+                    //greater than 100
+                    if urineSelection == 1 {
                         
-                        VStack(spacing: 40) {
-                            
-                            
-                            Text("Hyponatremia: Determine Cause")
-                                .modifier(MyTitleModifier(fillColor: fillColor))
-                            
-                            VStack {
-                                Text("Select plasma osmolality")
-                                    .font(.headline)
-                                
-                                Picker(selection: self.$plasmaSelection, label: Text("Plasma Osmolarity")) {
-                                    ForEach(0..<plasmaOsm.count) { item in
-                                        Text(self.plasmaOsm[item])
-                                    }
-                                }
-                                .modifier(SegmentedPickerModifier())
-                                
-                            }
-                            
-                            //only display options below "select serum osmolality
-                            //if plasmaSelection is option 0
-                            if self.plasmaSelection == 0 {
-                                
-                            
-                                VStack {
-                                    Text("Select urine osmolality")
-                                        .font(.headline)
-                                    Picker(selection: self.$urineSelection, label: Text("Volume status")) {
-                                        ForEach(0..<urineOsm.count) { item in
-                                            Text(self.urineOsm[item])
-                                                
-
-                                        }
-                                        
-                                    }
-                                    .modifier(SegmentedPickerModifier())
-                                    
-                                    
-                                }
-                                
-                                //only display these VStacks if urineOsmolality is
-                                //greater than 100
-                                if urineSelection == 1 {
-                                    VStack {
-                                        Text("Select volume status")
-                                            .font(.headline)
-                                        Picker(selection: self.$volumeSelection, label: Text("Volume status")) {
-                                            ForEach(0..<volumeStatus.count) { item in
-                                                Text(self.volumeStatus[item])
-                                            }
-                                        }
-                                        .modifier(SegmentedPickerModifier())
-                                        
-                                        
-                                    }
-                                    
-                                    //only display this VStack if not Euvolemic
-                                    //hyponatremia
-                                    if volumeSelection != 1 {
-                                        VStack {
-                                            Text("Select urine sodium")
-                                                .font(.headline)
-                                            Picker(selection: self.$urineNaSelection, label: Text("Urine sodium")) {
-                                                ForEach(0..<urineNa.count) { item in
-                                                    Text(self.urineNa[item])
-                                                }
-                                            }
-                                            .modifier(SegmentedPickerModifier())
-                                            
-                                        }
-                                    }
-                                }
-                                
-                            }
-                            
-                            
-                            
-                                Button(action: {
-                                    self.showingDetail.toggle()
-                                }) {
-                                    Text("Go!")
-                                        .frame(width: 50, height: 30, alignment: .center)
-                                }.sheet(isPresented: $showingDetail) {
-                                    if self.plasmaSelection == 0 {
-                                        if self.urineSelection == 0 {
-                                            LowUrineOsmView()
-                                        } else if self.urineSelection == 1 {
-                                            if self.volumeSelection == 0 {
-                                                if self.urineNaSelection == 0 {
-                                                    HypovolemicHyponatremiaView()
-                                                } else {
-                                                    HypovolemicHyponatremiaView2()
-                                                }
-                                            } else if self.volumeSelection == 1 {
-                                                EuvolemicHypoNaView()
-                                            } else if self.volumeSelection == 2 {
-                                                    HypervolemicHypoNaView()
-
-                                            }
-                                        }
-                                    } else if self.plasmaSelection  == 1 {
-                                        IsotonicHyponatremia()
-                                    } else {
-                                        HypertonicHyponatremia()
-                                    }
-
-                                } .buttonStyle(GradientButtonStyle())
-                                    
-                                Spacer()
-                            
-                            
-                            
-                            
-                            
+                        ShowPicker(parentBinding: $volumeSelection, text: "Select volume status", parentArray: volumeStatus)
+                        
+                        //only display this VStack if not Euvolemic
+                        //hyponatremia
+                        if volumeSelection != 1 {
+                            ShowPicker(parentBinding: $urineNaSelection, text: "Select urine sodium", parentArray: urineNa)
+                        }
+                    }
+                    
                 }
+                
+                
+                
+                Button(action: {
+                    self.showingDetail.toggle()
+                }) {
+                    Text("Go!")
+                        .frame(width: 50, height: 30, alignment: .center)
+                }.sheet(isPresented: $showingDetail) {
+                    if self.plasmaSelection == 0 {
+                        if self.urineSelection == 0 {
+                            LowUrineOsmView()
+                        } else if self.urineSelection == 1 {
+                            if self.volumeSelection == 0 {
+                                if self.urineNaSelection == 0 {
+                                    HypovolemicHyponatremiaView()
+                                } else {
+                                    HypovolemicHyponatremiaView2()
+                                }
+                            } else if self.volumeSelection == 1 {
+                                EuvolemicHypoNaView()
+                            } else if self.volumeSelection == 2 {
+                                HypervolemicHypoNaView()
+                                
+                            }
+                        }
+                    } else if self.plasmaSelection  == 1 {
+                        IsotonicHyponatremia()
+                    } else {
+                        HypertonicHyponatremia()
+                    }
+                    
+                } .buttonStyle(GradientButtonStyle())
+                
+                Spacer()
+                
+                
+                
+                
                 
             }
             
         }
         
+    }
+        
 }
 
 
+struct ShowPicker: View {
     
+    @Binding var parentBinding: Int
+    let text: String
+    let parentArray: [String]
+    
+    var body: some View {
+        VStack {
+            Text(text)
+                .font(.headline)
+            Picker(selection: $parentBinding, label: Text(text)) {
+                ForEach(0..<parentArray.count) { item in
+                    Text(self.parentArray[item])
+                }
+                
+            }
+            .modifier(SegmentedPickerModifier())
+        }
+    }
+}
 
-    
-    //methods
-    
- 
-    
 
 
 struct HyponatremiaView_Previews: PreviewProvider {
